d10b1590da3b1148d07b2eb5e4ae9da92a24aec2
NGorbanev <114814786+NGorbanev@users.noreply.github.com>
2022-11-01 10:38:06 +0300

    Update DepositCalculator.java
    Значимых замечаний по CodeStyle нет, минорные комментарии по тексту. Код действительно понятный, все задачи из теории выполнены

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 public class DepositCalculator  {
 
     public static void main (String[] args) {
-        new DepositCalculator ().calculationOfDeposit();
+        new DepositCalculator ().calculationOfDeposit(); //DepositCalculator () - пробел между названием класса и скобками кажется лишним
     }
 
     double calculateComplexPercent (double a, int d) {
         double pay = a * Math.pow((1 + 0.06 / 12), 12 * d);
 
-        return setRandom(pay);
+        return setRandom(pay); // пустая строка перед return вроде бы не нужна. В следующем блоке перед return пустой строки нет, а через один - снова есть
     }
 
     double calculateSimplePercent (double doubleAmount, int depositPeriod) {
@@ -36,7 +36,7 @@ public class DepositCalculator  {
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = scanner.nextInt();
 
-        double out = 0;
+        double out = 0; // не помню что про это писалось в теории, но вроде бы переменные лучше объявлять одним блоком. И у меня тоже вроде эта же ошибка есть ) 
 
         if (action == 1) {
             out = calculateSimplePercent(amount, period);
